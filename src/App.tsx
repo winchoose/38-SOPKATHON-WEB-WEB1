68e0aeebf25cebc32e131feee1e5c46ea416a747
winchoose <tmdxor741@naver.com>
2026-05-17 04:13:54 +0900

    feat: 라우터 구현( #40)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './routes/router';
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
