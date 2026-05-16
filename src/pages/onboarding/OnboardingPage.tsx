5e1c365fa160da819e855f7eaa33f3340f787670
winchoose <tmdxor741@naver.com>
2026-05-17 05:30:31 +0900

    feat: 모스 카드 구현( #23)

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -1,5 +1,17 @@
+import { MosCard } from '../../shared/components/MosCard';
+
 const OnboardingPage = () => {
-  return <div>온보딩</div>;
+  return (
+    <section>
+      <MosCard
+        className="mx-auto mt-[4rem]"
+        content="-.- . ..-. ..-. ... -.-"
+        sender="000"
+        receiver="000"
+        date="2025.05.17"
+      />
+    </section>
+  );
 };
 
 export default OnboardingPage;
--- src/shared/components/MosCard.tsx
+++ src/shared/components/MosCard.tsx
@@ -10,17 +10,19 @@ export const MosCard = ({
   content,
   sender,
   receiver,
-  date,
   className,
 }: MosCardProps) => {
   return (
-    <article className={`${className ?? ''}`}>
-      <span>from.{sender}</span>
-      <div>{content}</div>
-      <div>
-        <span>{date}</span>
-        <span>{receiver}</span>
+    <article
+      className={`border-sub-gray-2 flex flex-1 flex-col gap-[5px] rounded-[5px] border px-[13px] py-[6px] ${className ?? ''}`}
+    >
+      <span className="text-[14px]">from.{sender}</span>
+      <div className="mb-[20px] line-clamp-3 w-full text-[15px] text-black">
+        {content}
       </div>
+      <span className="text-sub-gray-2 text-end text-[14px]">
+        TO.{receiver}
+      </span>
     </article>
   );
 };
