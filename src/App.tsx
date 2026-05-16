a0ee021ecd32be1d1b75033a0fe62b334dbe4637
winchoose <tmdxor741@naver.com>
2026-05-17 03:47:05 +0900

    feat: input 구현( #25)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { useState } from 'react';
-import { Input } from './shared/components/Input';
-
 function App() {
-  const [receiver, setReceiver] = useState('asdsad');
-  const [sender, setSender] = useState('asdasddsa');
-
-  return (
-    <div>
-      <Input
-        receiver={receiver}
-        sender={sender}
-        onReceiverChange={(event) => setReceiver(event.target.value)}
-        onSenderChange={(event) => setSender(event.target.value)}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -1,4 +1,6 @@
 import type { ChangeEventHandler, ReactNode } from 'react';
+import userIconBlue from '../assets/svg/usericon_blue.svg';
+import userIconOrange from '../assets/svg/usericon_orange.svg';
 
 interface InputProps {
   sender: string;
@@ -16,14 +18,26 @@ export const Input = ({
   onReceiverChange,
 }: InputProps) => {
   return (
-    <div className="boder-b flex h-[48px] flex-1 items-center justify-center border-b px-[50px]">
-      <input
-        className="text-center"
-        value={receiver}
-        onChange={onReceiverChange}
-      />
-      <span>{centerIcon ?? '→'}</span>
-      <input className="text-center" value={sender} onChange={onSenderChange} />
+    <div className="border-background-gray flex h-[48px] flex-1 items-center justify-center border-b px-[50px]">
+      <label className="flex items-center gap-[10px]">
+        <img src={userIconOrange} alt="" />
+        <input
+          value={receiver}
+          onChange={onReceiverChange}
+          placeholder="수신자"
+          className="w-[3.7rem] text-center text-[14px]"
+        />
+      </label>
+      <span className="px-[40px]">{centerIcon ?? '→'}</span>
+      <label className="flex items-center gap-[10px]">
+        <img src={userIconBlue} alt="" />
+        <input
+          value={sender}
+          onChange={onSenderChange}
+          placeholder="발신자"
+          className="w-[3.7rem] text-center text-[14px]"
+        />
+      </label>
     </div>
   );
 };
